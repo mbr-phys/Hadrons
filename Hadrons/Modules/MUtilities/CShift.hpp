5110c7c03c8da67c9d49488d24958279d2438916
Matthew Black <misconshark@protonmail.com>
2025-01-08 11:25:54 +0000

    update cshift for both propagator and gauge fields

--- Hadrons/Modules/MUtilities/CShift.hpp
+++ Hadrons/Modules/MUtilities/CShift.hpp
@@ -1,3 +1,30 @@
+/*
+ * CShift.hpp, part of Hadrons (https://github.com/aportelli/Hadrons)
+ *
+ * Copyright (C) 2015 - 2022
+ *
+ * Author: Antonin Portelli <antonin.portelli@me.com>
+ * Author: Ryan Hill <rchrys.hill@gmail.com>
+ * Author: Matthew Black <matthewkblack@protonmail.com>
+ *
+ * Hadrons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hadrons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hadrons.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See the full license in the file "LICENSE" in the top level distribution 
+ * directory.
+ */
+
+/*  END LEGAL */
 #ifndef Hadrons_MUtilities_CShift_hpp_
 #define Hadrons_MUtilities_CShift_hpp_
 
@@ -8,7 +35,7 @@
 BEGIN_HADRONS_NAMESPACE
 
 /******************************************************************************
- *                         CShift                                 *
+ *                            Field CShift                                  *
  ******************************************************************************/
 BEGIN_MODULE_NAMESPACE(MUtilities)
 
@@ -16,15 +43,14 @@ class CShiftPar: Serializable
 {
 public:
     GRID_SERIALIZABLE_CLASS_MEMBERS(CShiftPar,
-                                    std::string, q,
+                                    std::string, field,
                                     std::string, shift);
 };
 
-template <typename FImpl>
+template <typename Field>
 class TCShift: public Module<CShiftPar>
 {
 public:
-    FERM_TYPE_ALIASES(FImpl,);
     // constructor
     TCShift(const std::string name);
     // destructor
@@ -38,28 +64,29 @@ public:
     virtual void execute(void);
 };
 
-MODULE_REGISTER_TMP(CShift, TCShift<FIMPL>, MUtilities);
+MODULE_REGISTER_TMP(CShiftGaugeField, TCShift<GIMPL::GaugeField>, MUtilities);
+MODULE_REGISTER_TMP(CShiftPropagatorField, TCShift<FIMPL::PropagatorField>, MUtilities);
 
 /******************************************************************************
- *                 TCShift implementation                             *
+ *                     TCShift implementation                          *
  ******************************************************************************/
 // constructor /////////////////////////////////////////////////////////////////
-template <typename FImpl>
-TCShift<FImpl>::TCShift(const std::string name)
+template <typename Field>
+TCShift<Field>::TCShift(const std::string name)
 : Module<CShiftPar>(name)
 {}
 
 // dependencies/products ///////////////////////////////////////////////////////
-template <typename FImpl>
-std::vector<std::string> TCShift<FImpl>::getInput(void)
+template <typename Field>
+std::vector<std::string> TCShift<Field>::getInput(void)
 {
-    std::vector<std::string> in = {par().q};
+    std::vector<std::string> in = {par().field};
     
     return in;
 }
 
-template <typename FImpl>
-std::vector<std::string> TCShift<FImpl>::getOutput(void)
+template <typename Field>
+std::vector<std::string> TCShift<Field>::getOutput(void)
 {
     std::vector<std::string> out = {getName()};
     
@@ -67,28 +94,28 @@ std::vector<std::string> TCShift<FImpl>::getOutput(void)
 }
 
 // setup ///////////////////////////////////////////////////////////////////////
-template <typename FImpl>
-void TCShift<FImpl>::setup(void)
+template <typename Field>
+void TCShift<Field>::setup(void)
 {
-    envCreate(PropagatorField, getName(), 1, envGetGrid(PropagatorField));
+    envCreateLat(Field, getName());
 }
 
 // execution ///////////////////////////////////////////////////////////////////
-template <typename FImpl>
-void TCShift<FImpl>::execute(void)
+template <typename Field>
+void TCShift<Field>::execute(void)
 {
-    // Set up shift variables
-    Coordinate coord = strToVec<int>(par().shift);
-    const PropagatorField& prop = envGet(PropagatorField, par().q);
-    PropagatorField&        out = envGet(PropagatorField, getName());
+    Coordinate xvec = strToVec<int>(par().shift);
+    LOG(Message) << "Shifting '" << par().field << "' by vector [" 
+                 << xvec[0] << " " << xvec[1] << " "
+                 << xvec[2] << " " << xvec[3] << "]." << std::endl;
 
-    // Execute CShift
-    out = prop;
-    for (int mu=0;mu<coord.size();mu++) 
-    {
-        int shift = coord[mu];
-        if (shift != 0)
-            out = Cshift(out,mu,shift);
+    auto &U   = envGet(Field, par().field);
+    auto &Utr = envGet(Field, getName());
+    Utr = U;
+    
+    for (int dir = 0; dir < Nd; dir++) {
+        int coord = xvec[dir];
+        if (coord != 0) Utr = Cshift(Utr,dir,coord);
     }
 }
 
